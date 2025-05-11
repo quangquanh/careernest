d2f2818ddfa6187b0fc9f3a4fe508a30081ffc0a
Your Name <you@example.com>
2025-05-11 15:27:30 +0000

    update

--- Source BE/CareerNest/src/main/java/com/nhc/CareerNest/config/security/SecurityConfiguration.java
+++ Source BE/CareerNest/src/main/java/com/nhc/CareerNest/config/security/SecurityConfiguration.java
@@ -18,9 +18,6 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.nhc.CareerNest.util.security.*;
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
@@ -63,22 +60,6 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://34.132.23.249:3000"); // hoặc http://localhost:3000 nếu chạy local
-        configuration.addAllowedOrigin("http://34.132.23.249"); // hoặc http://localhost:3000 nếu chạy local
-        configuration.addAllowedOrigin("https://careernest.click"); // hoặc http://localhost:3000 nếu chạy local
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(
             HttpSecurity http,
             CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
@@ -99,13 +80,13 @@ public class SecurityConfiguration {
 
         http
                 .csrf(c -> c.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers(whiteList).permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/jobs/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/companies/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/ws/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/ws/**").permitAll()
                                 .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
                         .authenticationEntryPoint(customAuthenticationEntryPoint))
@@ -115,3 +96,4 @@ public class SecurityConfiguration {
     }
 
 }
+
--- Source BE/CareerNest/src/main/java/com/nhc/CareerNest/config/websocket/WebSocketConfiguration.java
+++ Source BE/CareerNest/src/main/java/com/nhc/CareerNest/config/websocket/WebSocketConfiguration.java
@@ -27,7 +27,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("http://localhost:3000","https://careernest.click")
                 .withSockJS();
     }
 
